1a78b9b865cff8c5d6197fb36db462fdee36bd33
dhruvilxcode <dhruvilxcode@gmail.com>
2022-07-28 15:04:49 +0530

    files added for initial version.

--- js/script.js
+++ js/script.js
@@ -44,3 +44,29 @@ button.forEach(item => {
         cursor.classList.remove('hover');
     });
 })
+
+
+
+// add current year
+function setYearForCopyright(){
+    const currentYearElem = document.getElementById("currentYear")
+
+    const year = new Date().getFullYear()
+    currentYearElem.innerText = year;
+}
+setYearForCopyright();
+
+
+// nav full screen open btn
+function openFullScreenNav() {
+    const navFullScreen = document.getElementById("navFullScreen")
+    
+    navFullScreen.classList.remove("nav-fullscreen-close")
+    navFullScreen.classList.add("nav-fullscreen-open")
+}
+function closeFullScreenNav() {
+    const navFullScreen = document.getElementById("navFullScreen")
+    navFullScreen.classList.add("nav-fullscreen-close")
+    navFullScreen.classList.remove("nav-fullscreen-open")
+    
+}
